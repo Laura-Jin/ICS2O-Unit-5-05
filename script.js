1759c508a755a8c098c23bbac0951138874cd912
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-12 17:34:53 +0000

    added spaces

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ document.getElementById('calculate').addEventListener('click', buttonOne)
 function buttonOne () {
   number = document.getElementById('number').value
   number = parseInt(number)
-  number= number**2
+  number = number ** 2
   alert(number)
 }
